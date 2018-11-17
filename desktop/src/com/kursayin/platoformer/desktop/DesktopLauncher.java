9f80a900a5dc07dfb35602962114f1bc8b2db68a
vuvkar <vuvkar@hotmail.co.uk>
2018-11-17 21:26:25 +0400

    first commit

---
+++ desktop/src/com/kursayin/platoformer/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.kursayin.platoformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kursayin.platoformer.Platoformer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1920;
+		config.height = 1080;
+		new LwjglApplication(new Platoformer(), config);
+	}
+}
